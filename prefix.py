c62d51483ff0385a83877112e916f321778cc9a8
Pavithira99 <46491396+Pavithira99@users.noreply.github.com>
2019-07-02 22:07:00 +0530

    Update prefix.py

--- prefix.py
+++ prefix.py
@@ -6,12 +6,12 @@ for i in range(n):
 val=list1[0]
 length=len(val)
 for i in range(1,len(list1)):
-  if(val in list1[i]):
-    continue
-  else:
-    
+  if(val not in list1[i]):
     val=val[:length-1]
     length-=1
+  else:
+    
+    continue
 print(val)
     
     
