ce52acf0f8aa6747e873aa74fce69a82bd446a53
Pavithira99 <46491396+Pavithira99@users.noreply.github.com>
2019-07-03 09:56:32 +0530

    Update repeated.py

--- repeated.py
+++ repeated.py
@@ -1,12 +1,12 @@
 n=int(input())
-unique=[]
+unique1=[]
 arr=list(map(int,input().split(" "))
 for i in range(0,len(arr)):
   if(arr.count(i)>1):
-    unique.append(i)
-unique.sort()
+    unique1.append(i)
+unique1.sort()
 new=""
-new=[str(i) for i in unique)
+new=[str(i) for i in unique1)
 res=" ".join(new)
 print(res)
  
