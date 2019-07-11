9d005e40718f57c678d47f001f95b34a23779acb
Pavithira P M <46491396+Pavithira99@users.noreply.github.com>
2019-07-11 17:52:18 +0530

    Update pro_6.py

--- pro_6.py
+++ pro_6.py
@@ -1,11 +1,11 @@
 n=int(input())
 if(n<=3):
   x=list(map(int,input().split(" ")))
-x.append(0)
-x.append(0)
-count=0
-for i in range(0,len(x)-2):
-  if(x[i]<x[i+1]):
-    if(x[i+1]<x[i+2]):
-      count+=1
-print(count)
+  x.append(0)
+  x.append(0)
+  count=0
+  for i in range(0,len(x)-2):
+    if(x[i]<x[i+1]):
+      if(x[i+1]<x[i+2]):
+        count+=1
+  print(count)
