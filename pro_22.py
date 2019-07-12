5559c0e1a66a0d9f825896b705c33223056f2877
Pavithira P M <46491396+Pavithira99@users.noreply.github.com>
2019-07-12 17:02:43 +0530

    Update pro_22.py

--- pro_22.py
+++ pro_22.py
@@ -1,6 +1,12 @@
 n=int(input())
 arr=list(map(int,input().split()))
 sum=0
-for i in range(0,len(arr)):
+list1=[]
+for i in range(0,len(arr),2):
   sum+=arr[i]
-print(sum)
+  list1.append(sum)
+sum=0
+for i in range(1,len(arr),2):
+  sum+=arr[i]
+  list1.append(sum)
+print(max(list1))
