c6246c630281e9531323f12ee0d29ed000cb9051
Pavithira P M <46491396+Pavithira99@users.noreply.github.com>
2019-07-12 16:53:39 +0530

    Create pro_12.py

---
+++ pro_12.py
@@ -0,0 +1,13 @@
+n=list(map(int,input().split()))
+arr=list(map(int,input().split()))
+list1=[]
+list2=[]
+for i in range(0,n):
+  x=list(map(int,input().split()))
+  list1.append(x[0])
+  list2.append(x[1])
+for i in range(0,len(list1)):
+  sum=0
+  for j in range(list1[i],list2[i]):
+    sum+=arr[j]
+  print(sum)
