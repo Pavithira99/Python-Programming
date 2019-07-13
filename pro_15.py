b715bab6da578461792bf0ab0c3a33cf96db8c03
Pavithira P M <46491396+Pavithira99@users.noreply.github.com>
2019-07-13 15:18:50 +0530

    Update pro_15.py

--- pro_15.py
+++ pro_15.py
@@ -1,6 +1,9 @@
 n=int(input())
 arr=list(map(int,input().split()))
+arr=[bin(i) for i in arr]
+arr.sort(reverse=True)
+arr=[int(x,2) for x in arr]
+
 for i in range(0,n):
-  print(max(arr))
-  arr.remove(max(arr))
+  print(arr[i])
   
