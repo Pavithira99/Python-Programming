aacd013f5a642354756aca82253199ac7d61ae49
Pavithira P M <46491396+Pavithira99@users.noreply.github.com>
2019-07-13 16:11:23 +0530

    Update pro_17.py

--- pro_17.py
+++ pro_17.py
@@ -1,11 +1,14 @@
 n=list(map(int,input().split()))
+x=n[1]
 arr=list(map(int,input().split()))
 arr.append(0)
 flag=1
-for i in range(0,n[0]-1):
-  if(arr[i]+arr[i+1]==n[1]):
-    print("yes")
+for i in range(0,len(arr)-1):
+  if(arr[i]+arr[i+1]==x):
+    
     flag=0
     break
 if(flag==1):
   print("no")
+else:
+  print("yes")
